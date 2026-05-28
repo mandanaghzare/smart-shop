8ff6ea6e69cdd9c8cfef74c54eeeda1d08fe3817
mandana <mandana.ghzare@gmail.com>
2026-05-28 21:08:47 +0200

    protect profile page with auth guard

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,8 +1,16 @@
+"use client"
+import { useAuthStore } from "@/store/authStore"
 
 const ProfilePage = () => {
+    const user = useAuthStore((state) => state.user)
+
+    if(!user) {
+        return <div>Please login to view your profile</div>
+    }
     return(
         <div>
-            <h1>Cart Page</h1>
+            <h1>Profile Page</h1>
+            <p>Email: {user.email}</p>
         </div>
     )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,11 @@
+"use client"
+import { useAuthStore } from "@/store/authStore";
 import Link from "next/link";
 
 
 const Navbar = () => {
+    const user = useAuthStore((state) => state.user)
+    const logout = useAuthStore((state) => state.logout)
     return(
         <nav className="border-b border-gray-200 bg-white px-8 py-4 shadow-sm">
             <div className="mx-auto flex max-w-6xl items-center justify-between">
@@ -19,7 +23,24 @@ const Navbar = () => {
                         <Link href="/cart" className="transition hover:text-gray-950">Cart</Link>
                     </li>
                     <li>
-                        <Link href="/login" className="transition hover:text-gray-950">Login</Link>
+                        {user ? (
+                            <div className="flex items-center gap-6">
+                                <Link href="/profile" className="transition hover:text-gray-950">
+                                Profile
+                                </Link>
+
+                                <button
+                                onClick={logout}
+                                className="transition cursor-pointer hover:text-gray-950"
+                                >
+                                Logout
+                                </button>
+                            </div>
+                            ) : (
+                            <Link className="text-sm text-gray-700 hover:text-black" href="/login">
+                                Login
+                            </Link>
+                        )}
                     </li>
                 </ul>                
             </div>
